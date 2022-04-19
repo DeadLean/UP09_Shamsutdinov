7518df31d7f71d28aa96a0627a3b8e30f35434b7
DeadLean <linar123.ls@gmail.com>
2022-04-19 11:58:18 +0300

    try modalWindow

---
+++ script.js
@@ -0,0 +1,8 @@
+import modalAppearance from "./js/modalOpen.js";
+import modalClose from "./js/modalClose.js";
+import formValidation from "./js/formValidation.js";
+
+
+modalAppearance();
+modalClose();
+formValidation();
