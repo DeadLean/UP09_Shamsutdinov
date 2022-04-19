8cc1887da30358fbd45c7f9f939fb32fa696410d
DeadLean <linar123.ls@gmail.com>
2022-04-19 12:31:57 +0300

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import modalAppearance from "./modules/modalOpen.js";
-import modalClose from "./modules/modalClose.js";
-import formValidation from "./modules/formValidation.js";
-import accordionsOpening from "./modules/accordionsOpening.js";
-import sendingForm from "./modules/sendingForm.js";
-import constructorAccordion from "./modules/constructorAccordion.js";
+import {modalAppearance} from "./modules/modalOpen.js";
+import {modalClose} from "./modules/modalClose.js";
+import {formValidation} from "./modules/formValidation.js";
+import {accordionsOpening} from "./modules/accordionsOpening.js";
+import {sendingForm} from "./modules/sendingForm.js";
+import {constructorAccordion} from "./modules/constructorAccordion.js";
 
 modalAppearance();
 modalClose();
